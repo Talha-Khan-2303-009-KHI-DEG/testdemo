3c53a5d0332738a9041a30e3b17823b7a9fbf2dc
Talha-Khan-2303-009-KHI-DEG <124855680+Talha-Khan-2303-009-KHI-DEG@users.noreply.github.com>
2023-02-09 12:53:50 +0500

    route added
    new route added

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print("hello world")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def test():
+    return "Hello World"
+
+@app.route('/another_route')
+def another_route():
+    return "Hello xloop"
+
+if __name__=="__main__":
+    app.run(debug=True)
